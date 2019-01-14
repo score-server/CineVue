b0dda9037e95367e24e04c6d0244cd22c430be68
Wetwer <felix.spoerri@gmail.com>
2019-01-14 10:56:28 +0100

    Renamed

--- src/main.module.js
+++ src/main.module.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './app.module'
-import Router from './' //TODO rename index.js to router.config.js and main.js to main.module.js, fix import paths
+import Router from './' //TODO rename index.js to router.config.js and main.module.js to app.module.js, fix import paths
 
 Vue.config.productionTip = false;
 
